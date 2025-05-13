816a1fba41edc372e0460c530bd0821965ec6e2c
Kuala_Notebook <kminsung3362@gmail.com>
2025-05-13 10:42:08 +0900

    stop 구현

--- src/main/java/com/example/demo/MusicService.java
+++ src/main/java/com/example/demo/MusicService.java
@@ -18,21 +18,25 @@ public class MusicService {
     
     public boolean play(String filename) {
         try {
-            File file = new File(musicFolderPath + filename + ".mp3");
+        	if (!filename.endsWith(".mp3") && !filename.endsWith(".wav")) {
+                filename += ".mp3";
+            }
+            File file = new File(musicFolderPath + "/" + filename);
             if (!file.exists()) {
                 System.out.println("파일 없음: " + file.getAbsolutePath());
                 return false;
             }
-
-            FileInputStream fis = new FileInputStream(file);
-            Player player = new Player(fis);
-            new Thread(() -> {
-                try {
-                    player.play();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }).start();
+            if (filename.endsWith(".mp3")) {
+            	FileInputStream fis = new FileInputStream(file);
+            	this.player = new Player(fis);
+            	new Thread(() -> {
+            		try {
+            			player.play();
+            		} catch (Exception e) {
+            			e.printStackTrace();
+            		}
+            	}).start();
+            }
             return true;
         } catch (Exception e) {
             e.printStackTrace();
